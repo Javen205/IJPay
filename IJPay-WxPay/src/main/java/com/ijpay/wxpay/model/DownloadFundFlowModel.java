381e6db8641f3d892d9055a130436b7ec6da2b0d
Javen205 <javen205@126.com>
2019-07-26 23:08:09 +0800

    :sparkles: 添加各种支付的 Model

---
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/model/DownloadFundFlowModel.java
@@ -0,0 +1,33 @@
+/**
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付常用的支付方式以及各种常用的接口。</p>
+ *
+ * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
+ *
+ * <p>IJPay 交流群: 723992875</p>
+ *
+ * <p>Node.js 版: https://gitee.com/javen205/TNW</p>
+ *
+ * <p>下载资金账单 Model</p>
+ *
+ * @author Javen
+ */
+package com.ijpay.wxpay.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class DownloadFundFlowModel extends BaseModel {
+    private String appid;
+    private String mch_id;
+    private String nonce_str;
+    private String sign;
+    private String sign_type;
+    private String bill_date;
+    private String account_type;
+    private String tar_type;
+}
---
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/model/ReportModel.java
@@ -0,0 +1,43 @@
+/**
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付常用的支付方式以及各种常用的接口。</p>
+ *
+ * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
+ *
+ * <p>IJPay 交流群: 723992875</p>
+ *
+ * <p>Node.js 版: https://gitee.com/javen205/TNW</p>
+ *
+ * <p>交易保障 Model</p>
+ *
+ * @author Javen
+ */
+package com.ijpay.wxpay.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class ReportModel extends BaseModel{
+    private String appid;
+    private String sub_appid;
+    private String mch_id;
+    private String sub_mch_id;
+    private String device_info;
+    private String nonce_str;
+    private String sign;
+    private String sign_type;
+    private String interface_url;
+    private String execute_time;
+    private String return_code;
+    private String return_msg;
+    private String result_code;
+    private String err_code;
+    private String err_code_des;
+    private String out_trade_no;
+    private String user_ip;
+    private String time;
+}
