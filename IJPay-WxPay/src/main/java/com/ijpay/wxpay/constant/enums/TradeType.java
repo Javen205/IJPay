454cfadcc02559a2a0a909002594964006e46d79
Javen205 <javen205@126.com>
2019-07-27 13:34:21 +0800

    Revert ":bulb: 注释中添加群信息"

--- IJPay-WxPay/src/main/java/com/ijpay/wxpay/constant/enums/TradeType.java
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/constant/enums/TradeType.java
@@ -5,20 +5,16 @@ package com.ijpay.wxpay.constant.enums;
  *
  * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
  *
- * <p>IJPay 交流群: 723992875</p>
- *
- * <p>Node.js 版: https://gitee.com/javen205/TNW</p>
- *
  * <p>支付方式</p>
  *
  * @author Javen
  */
 public enum TradeType {
-    JSAPI("JSAPI", "微信公众号支付或者小程序支付）"),
-    NATIVE("NATIVE", "微信扫码支付"),
-    APP("APP", "微信APP支付"),
-    MICROPAY("MICROPAY", "付款码支付"),
-    MWEB("MWEB", "H5支付");
+    JSAPI("JSAPI","微信公众号支付或者小程序支付）"),
+    NATIVE("NATIVE","微信扫码支付"),
+    APP("APP","微信APP支付"),
+    MICROPAY("MICROPAY","付款码支付"),
+    MWEB("MWEB","H5支付");
 
     /**
      * 交易类型枚举<br/>
