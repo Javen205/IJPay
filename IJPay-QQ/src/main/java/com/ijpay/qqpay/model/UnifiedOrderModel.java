0c1be911a4cc4cdd4d4557d4278ea336cb620eb3
Javen205 <javendev@126.com>
2019-08-03 18:22:15 +0800

    :sparkles: 添加 QQ 钱包支付

---
+++ IJPay-QQ/src/main/java/com/ijpay/qqpay/model/UnifiedOrderModel.java
@@ -0,0 +1,48 @@
+/**
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付常用的支付方式以及各种常用的接口。</p>
+ *
+ * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
+ *
+ * <p>IJPay 交流群: 723992875</p>
+ *
+ * <p>Node.js 版: https://gitee.com/javen205/TNW</p>
+ *
+ * <p>统一下单 Model</p>
+ *
+ * @author Javen
+ */
+package com.ijpay.qqpay.model;
+
+import com.ijpay.core.model.BaseModel;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class UnifiedOrderModel extends BaseModel {
+    private String appid;
+    private String sub_appid;
+    private String mch_id;
+    private String sub_mch_id;
+    private String nonce_str;
+    private String sign;
+    private String body;
+    private String attach;
+    private String out_trade_no;
+    private String fee_type;
+    private String total_fee;
+    private String spbill_create_ip;
+    private String time_start;
+    private String time_expire;
+    private String limit_pay;
+    private String contract_code;
+    private String promotion_tag;
+    private String trade_type;
+    private String notify_url;
+    private String device_info;
+    private String mini_app_param;
+}
