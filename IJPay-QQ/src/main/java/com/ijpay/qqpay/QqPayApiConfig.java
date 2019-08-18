bf65dc3f9430cceb3746568de38caa6da1d4f211
Javen205 <javendev@126.com>
2019-08-18 15:41:49 +0800

    :sparkles: 支持多应用

---
+++ IJPay-QQ/src/main/java/com/ijpay/qqpay/QqPayApiConfig.java
@@ -0,0 +1,34 @@
+package com.ijpay.qqpay;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付常用的支付方式以及各种常用的接口。</p>
+ *
+ * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
+ *
+ * <p>IJPay 交流群: 723992875</p>
+ *
+ * <p>Node.js 版: https://gitee.com/javen205/TNW</p>
+ *
+ * <p>QQ 钱包支付常用配置</p>
+ *
+ * @author Javen
+ */
+
+@Getter
+@Setter
+public class QqPayApiConfig implements Serializable {
+    private static final long serialVersionUID = 8365822256469245771L;
+    
+    private String appId;
+    private String mchId;
+    private String subAppId;
+    private String subMchId;
+    private String partnerKey;
+    private String domain;
+    private String certPath;
+}
