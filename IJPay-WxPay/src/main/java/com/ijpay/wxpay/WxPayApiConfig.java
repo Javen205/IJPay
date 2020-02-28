8b6de4bdc91bf94fb03e44c9c2660aa2d3cbe8b7
Javen205 <javendev@126.com>
2020-02-28 14:42:39 +0800

    :sparkles: 添加 Api-v3 接口参数并添加注释

--- IJPay-WxPay/src/main/java/com/ijpay/wxpay/WxPayApiConfig.java
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/WxPayApiConfig.java
@@ -3,6 +3,7 @@ package com.ijpay.wxpay;
 import lombok.*;
 
 import java.io.Serializable;
+
 /**
  * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付常用的支付方式以及各种常用的接口。</p>
  *
@@ -23,13 +24,52 @@ import java.io.Serializable;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class WxPayApiConfig implements Serializable {
     private static final long serialVersionUID = -9044503427692786302L;
-    
+    /**
+     * 应用编号
+     */
     private String appId;
+    /**
+     * 商户号
+     */
     private String mchId;
+    /**
+     * 服务商应用编号
+     */
     private String slAppId;
+    /**
+     * 服务商商户号
+     */
     private String slMchId;
+    /**
+     * 同 apiKey 后续版本会舍弃
+     */
     private String partnerKey;
+    /**
+     * 商户平台「API安全」中的 API 密钥
+     */
+    private String apiKey;
+    /**
+     * 商户平台「API安全」中的 APIv3 密钥
+     */
+    private String apiKey3;
+    /**
+     * 应用域名，回调中会使用此参数
+     */
     private String domain;
+    /**
+     * API 证书中的 p12
+     */
     private String certPath;
+    /**
+     * API 证书中的 key.pem
+     */
+    private String keyPemPath;
+    /**
+     * API 证书中的 cert.pem
+     */
+    private String certPemPath;
+    /**
+     * 其他附加参数
+     */
     private Object exParams;
 }
