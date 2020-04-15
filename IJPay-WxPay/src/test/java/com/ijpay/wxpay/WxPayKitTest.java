80e69979e6aefc06a833ef071eeba7d725562244
Javen205 <javendev@126.com>
2020-04-15 16:01:58 +0800

    :white_check_mark: 添加接口拼接测试

--- IJPay-WxPay/src/test/java/com/ijpay/wxpay/WxPayKitTest.java
+++ IJPay-WxPay/src/test/java/com/ijpay/wxpay/WxPayKitTest.java
@@ -2,6 +2,8 @@ package com.ijpay.wxpay;
 
 
 import com.ijpay.core.kit.WxPayKit;
+import com.ijpay.wxpay.enums.WxApiType;
+import com.ijpay.wxpay.enums.WxDomain;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,7 +16,7 @@ public class WxPayKitTest {
     }
 
     @Test
-    public void mad5() {
+    public void md5() {
         Assert.assertEquals("cbfc2149d454ecf4ab0f32e58430fcdd",
                 WxPayKit.md5("IJPay"));
     }
@@ -31,5 +33,11 @@ public class WxPayKitTest {
                 WxPayKit.decryptData(
                         WxPayKit.encryptData("IJPay", "42cc1d91bab89b65ff55b19e28fff4f0"),
                         "42cc1d91bab89b65ff55b19e28fff4f0"));
+    }
+
+    @Test
+    public void StringFormat() {
+        Assert.assertEquals("https://api.mch.weixin.qq.com/v3/discount-card/orders/out-trade-no/123456789",
+               String.format(WxDomain.CHINA.toString().concat(WxApiType.DISCOUNT_CARD_ORDER_TRADE_NO.toString()),"123456789"));
     }
 }
