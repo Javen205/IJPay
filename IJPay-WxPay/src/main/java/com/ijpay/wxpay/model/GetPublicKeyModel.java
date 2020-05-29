544c9c0924a28f0517dc3728853951238ca7fd3f
Javen205 <javendev@126.com>
2020-05-29 12:52:46 +0800

    :sparkles: 新增获取 RSA 加密公钥接口请求参数的 Model

---
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/model/GetPublicKeyModel.java
@@ -0,0 +1,31 @@
+package com.ijpay.wxpay.model;
+
+import com.ijpay.core.model.BaseModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付、PayPal 等常用的支付方式以及各种常用的接口。</p>
+ *
+ * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
+ *
+ * <p>IJPay 交流群: 723992875</p>
+ *
+ * <p>Node.js 版: https://gitee.com/javen205/TNWX</p>
+ *
+ * <p>获取RSA加密公钥 Model</p>
+ *
+ * @author Javen
+ */
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+public class GetPublicKeyModel extends BaseModel {
+    private String mch_id;
+    private String nonce_str;
+    private String sign;
+    private String sign_type;
+}
