2a7739df18c011573531ee581bce11f81f8d6d18
Javen205 <javendev@126.com>
2021-10-05 16:54:04 +0800

    :sparkles: 支付宝证书模式支持证书内容字符串 #I4842W

--- IJPay-AliPay/src/main/java/com/ijpay/alipay/AliPayApiConfig.java
+++ IJPay-AliPay/src/main/java/com/ijpay/alipay/AliPayApiConfig.java
@@ -33,8 +33,11 @@ public class AliPayApiConfig implements Serializable {
     private String format;
     private boolean certModel;
     private String appCertPath;
+    private String appCertContent;
     private String aliPayCertPath;
+    private String aliPayCertContent;
     private String aliPayRootCertPath;
+    private String aliPayRootCertContent;
     private AlipayClient alipayClient;
 
     private AliPayApiConfig() {
@@ -66,6 +69,16 @@ public class AliPayApiConfig implements Serializable {
     }
 
     /**
+     * 证书模式
+     *
+     * @return AliPayApiConfig 支付宝配置
+     * @throws AlipayApiException 支付宝 Api 异常
+     */
+    public AliPayApiConfig buildByCertContent() throws AlipayApiException {
+        return buildByCertContent(getAppCertContent(), getAliPayCertContent(), getAliPayRootCertContent());
+    }
+
+    /**
      * @param appCertPath        应用公钥证书路径
      * @param aliPayCertPath     支付宝公钥证书文件路径
      * @param aliPayRootCertPath 支付宝CA根证书文件路径
@@ -83,6 +96,29 @@ public class AliPayApiConfig implements Serializable {
         certAlipayRequest.setCertPath(appCertPath);
         certAlipayRequest.setAlipayPublicCertPath(aliPayCertPath);
         certAlipayRequest.setRootCertPath(aliPayRootCertPath);
+        this.alipayClient = new DefaultAlipayClient(certAlipayRequest);
+        this.certModel = true;
+        return this;
+    }
+
+    /**
+     * @param appCertContent        应用公钥证书
+     * @param aliPayCertContent     支付宝公钥证书
+     * @param aliPayRootCertContent 支付宝CA根证书
+     * @return {@link AliPayApiConfig}  支付宝支付配置
+     * @throws AlipayApiException 支付宝 Api 异常
+     */
+    public AliPayApiConfig buildByCertContent(String appCertContent, String aliPayCertContent, String aliPayRootCertContent) throws AlipayApiException {
+        CertAlipayRequest certAlipayRequest = new CertAlipayRequest();
+        certAlipayRequest.setServerUrl(getServiceUrl());
+        certAlipayRequest.setAppId(getAppId());
+        certAlipayRequest.setPrivateKey(getPrivateKey());
+        certAlipayRequest.setFormat(getFormat());
+        certAlipayRequest.setCharset(getCharset());
+        certAlipayRequest.setSignType(getSignType());
+        certAlipayRequest.setCertContent(appCertContent);
+        certAlipayRequest.setAlipayPublicCertContent(aliPayCertContent);
+        certAlipayRequest.setRootCertContent(aliPayRootCertContent);
         this.alipayClient = new DefaultAlipayClient(certAlipayRequest);
         this.certModel = true;
         return this;
@@ -188,6 +224,15 @@ public class AliPayApiConfig implements Serializable {
         return this;
     }
 
+    public String getAppCertContent() {
+        return appCertContent;
+    }
+
+    public AliPayApiConfig setAppCertContent(String appCertContent) {
+        this.appCertContent = appCertContent;
+        return this;
+    }
+
     public String getAliPayCertPath() {
         return aliPayCertPath;
     }
@@ -197,12 +242,30 @@ public class AliPayApiConfig implements Serializable {
         return this;
     }
 
+    public String getAliPayCertContent() {
+        return aliPayCertContent;
+    }
+
+    public AliPayApiConfig setAliPayCertContent(String aliPayCertContent) {
+        this.aliPayCertContent = aliPayCertContent;
+        return this;
+    }
+
     public String getAliPayRootCertPath() {
         return aliPayRootCertPath;
     }
 
     public AliPayApiConfig setAliPayRootCertPath(String aliPayRootCertPath) {
         this.aliPayRootCertPath = aliPayRootCertPath;
+        return this;
+    }
+
+    public String getAliPayRootCertContent() {
+        return aliPayRootCertContent;
+    }
+
+    public AliPayApiConfig setAliPayRootCertContent(String aliPayRootCertContent) {
+        this.aliPayRootCertContent = aliPayRootCertContent;
         return this;
     }
 
