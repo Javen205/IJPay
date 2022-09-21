c5ce92e9995491d116d0bedd61936afdb04dbe92
Javen205 <javendev@126.com>
2022-09-21 08:43:53 +0800

    :zap: 调整域名枚举

--- IJPay-WxPay/src/main/java/com/ijpay/wxpay/enums/WxDomain.java
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/enums/WxDomain.java
@@ -1,7 +1,7 @@
 package com.ijpay.wxpay.enums;
 
 /**
- * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付常用的支付方式以及各种常用的接口。</p>
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付、PayPal 等常用的支付方式以及各种常用的接口。</p>
  *
  * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
  *
@@ -9,58 +9,15 @@ package com.ijpay.wxpay.enums;
  *
  * <p>Node.js 版: https://gitee.com/javen205/TNWX</p>
  *
- * <p>微信支付可用域名枚举</p>
+ * <p>微信域名枚举接口</p>
  *
  * @author Javen
  */
-public enum WxDomain implements WxDomainEnum{
+public interface WxDomain {
     /**
-     * 中国国内
+     * 获取域名
+     *
+     * @return 返回域名
      */
-    CHINA("https://api.mch.weixin.qq.com"),
-    /**
-     * 中国国内(备用域名)
-     */
-    CHINA2("https://api2.mch.weixin.qq.com"),
-    /**
-     * 东南亚
-     */
-    HK("https://apihk.mch.weixin.qq.com"),
-    /**
-     * 其它
-     */
-    US("https://apius.mch.weixin.qq.com"),
-    /**
-     * 获取公钥
-     */
-    FRAUD("https://fraud.mch.weixin.qq.com"),
-    /**
-     * 活动
-     */
-    ACTION("https://action.weixin.qq.com"),
-    /**
-     * 刷脸支付
-     * PAY_APP
-     */
-    PAY_APP("https://payapp.weixin.qq.com");
-
-
-    /**
-     * 域名
-     */
-    private final String domain;
-
-    WxDomain(String domain) {
-        this.domain = domain;
-    }
-
-    @Override
-    public String getDomain() {
-        return domain;
-    }
-
-    @Override
-    public String toString() {
-        return domain;
-    }
+    String getDomain();
 }
--- IJPay-WxPay/src/main/java/com/ijpay/wxpay/enums/WxDomainEnum.java
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/enums/WxDomainEnum.java
@@ -1,23 +1,66 @@
 package com.ijpay.wxpay.enums;
 
 /**
- * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付、PayPal 等常用的支付方式以及各种常用的接口。</p>
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付常用的支付方式以及各种常用的接口。</p>
  *
  * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
  *
  * <p>IJPay 交流群: 723992875</p>
  *
- * <p>Node.js 版: https://gitee.com/javen205/TNWX</p>
+ * <p>Node.js 版: <a href="https://gitee.com/javen205/TNWX">Node.js 版</a></p>
  *
- * <p>微信域名枚举接口</p>
+ * <p>微信支付可用域名枚举</p>
  *
  * @author Javen
  */
-public interface WxDomainEnum {
+public enum WxDomainEnum implements WxDomain {
     /**
-     * 获取域名
-     *
-     * @return 返回域名
+     * 中国国内
      */
-    String getDomain();
+    CHINA("https://api.mch.weixin.qq.com"),
+    /**
+     * 中国国内(备用域名)
+     */
+    CHINA2("https://api2.mch.weixin.qq.com"),
+    /**
+     * 东南亚
+     */
+    HK("https://apihk.mch.weixin.qq.com"),
+    /**
+     * 其它
+     */
+    US("https://apius.mch.weixin.qq.com"),
+    /**
+     * 获取公钥
+     */
+    FRAUD("https://fraud.mch.weixin.qq.com"),
+    /**
+     * 活动
+     */
+    ACTION("https://action.weixin.qq.com"),
+    /**
+     * 刷脸支付
+     * PAY_APP
+     */
+    PAY_APP("https://payapp.weixin.qq.com");
+
+
+    /**
+     * 域名
+     */
+    private final String domain;
+
+    WxDomainEnum(String domain) {
+        this.domain = domain;
+    }
+
+    @Override
+    public String getDomain() {
+        return domain;
+    }
+
+    @Override
+    public String toString() {
+        return domain;
+    }
 }
