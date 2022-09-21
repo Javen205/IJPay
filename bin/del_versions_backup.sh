b56be8919c9912025a360d5f9d4403a556e66401
Javen205 <javendev@126.com>
2022-09-21 19:50:10 +0800

    :sparkles: 新增脚本删除 module 下的 pom.xml.versionsBackup

---
+++ bin/del_versions_backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#------------------------------------------------
+# 删除 module 下的 pom.xml.versionsBackup
+#------------------------------------------------
+
+rm -rf pom.xml.versionsBackup */pom.xml.versionsBackup
