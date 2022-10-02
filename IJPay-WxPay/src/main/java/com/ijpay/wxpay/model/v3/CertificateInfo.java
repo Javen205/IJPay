10c11a4f7d52216e664695cf574567beff23cc0a
Javen205 <javendev@126.com>
2022-10-02 15:58:37 +0800

    :sparkles: 添加获取证书列表实体类

---
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/model/v3/CertificateInfo.java
@@ -0,0 +1,37 @@
+package com.ijpay.wxpay.model.v3;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付等常用的支付方式以及各种常用的接口。</p>
+ *
+ * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
+ *
+ * <p>IJPay 交流群: 723992875、864988890</p>
+ *
+ * <p>Node.js 版: <a href="https://gitee.com/javen205/TNWX">https://gitee.com/javen205/TNWX</a></p>
+ *
+ * <p>证书响应参数 Model</p>
+ *
+ * @author Javen
+ */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class CertificateInfo implements Serializable {
+
+	private static final long serialVersionUID = -5838236796703654715L;
+
+	private String serial_no;
+	private String effective_time;
+	private String expire_time;
+	private EncryptCertificate encrypt_certificate;
+}
---
+++ IJPay-WxPay/src/main/java/com/ijpay/wxpay/model/v3/EncryptCertificate.java
@@ -0,0 +1,35 @@
+package com.ijpay.wxpay.model.v3;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * <p>IJPay 让支付触手可及，封装了微信支付、支付宝支付、银联支付等常用的支付方式以及各种常用的接口。</p>
+ *
+ * <p>不依赖任何第三方 mvc 框架，仅仅作为工具使用简单快速完成支付模块的开发，可轻松嵌入到任何系统里。 </p>
+ *
+ * <p>IJPay 交流群: 723992875、864988890</p>
+ *
+ * <p>Node.js 版: <a href="https://gitee.com/javen205/TNWX">https://gitee.com/javen205/TNWX</a></p>
+ *
+ * @author Javen
+ */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class EncryptCertificate implements Serializable {
+
+	private static final long serialVersionUID = -5718842307268149730L;
+
+	private String algorithm;
+	private String nonce;
+	private String associated_data;
+	private String ciphertext;
+}
